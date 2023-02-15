02c0f4bd02fa626f4cfbde5193eef9b9879862b9
NicoloDePietro <nicolo.depietro@issgreppi.it>
2023-02-15 09:06:29 +0100

    Edit e delete category

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace BulkyBookWeb.Models;
 public class Category
 {
@@ -6,6 +7,9 @@ public class Category
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
-    public int DisplayOrder { get; set; }
+	[DisplayName("Display Order")]
+	[Range(1, 100, ErrorMessage = "{0} must be between {1} and {2}")]
+
+	public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
