d9f0c5b12477639dc400638a0f0925c14f8bdefb
NICOLO' DE PIETRO <NICOLO.DEPIETRO@issgreppi.it>
2023-03-09 09:42:23 +0100

    aggiunta IProductRepository e l'altro senza I

---
+++ BulkyBook.DataAccess1/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBook.DataAccess1.Repository.IRepository;
+using BulkyBook.Models1;
+using BulkyBookWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess1.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product product)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = product.Title;
+                objFromDb.ISBN = product.ISBN;
+                objFromDb.Price = product.Price;
+                objFromDb.Price50 = product.Price50;
+                objFromDb.ListPrice = product.ListPrice;
+                objFromDb.Price100 = product.Price100;
+                objFromDb.Description = product.Description;
+                objFromDb.CategoryId = product.CategoryId;
+                objFromDb.Author = product.Author;
+                objFromDb.CoverTypeId = product.CoverTypeId;
+                if (product.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = product.ImageUrl;
+                }
+            }
+
+        }
+    }
+
+}
