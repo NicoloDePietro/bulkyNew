20d19fb0307328ab92bb7a0b5e4ba9f35a3846a1
NICOLO' DE PIETRO <NICOLO.DEPIETRO@issgreppi.it>
2023-03-21 10:19:35 +0100

    fino a 1.2.13.6.8.5

--- BulkyBook.DataAccess1/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess1/Repository/IRepository/IRepository.cs
@@ -10,8 +10,9 @@ namespace BulkyBook.DataAccess1.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
-        T? GetFirstOrDefault(Expression<Func<T, bool>> filter);
-        IEnumerable<T> GetAll();
+        T? GetFirstOrDefault(Expression<Func<T, bool>> filter, string? inludeProperties = null);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
---
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -0,0 +1,21 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    //per i dettagli si veda qui: https://datatables.net/manual/ajax
+    dataTable = $('#tblData').DataTable({
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.13.2/i18n/it-IT.json"
+        },
+        ajax: {
+            url: "/Admin/Product/GetAll"
+        },
+        columns: [
+            { data: "title", width: "15%" },
+            { data: "isbn", width: "15%" },
+            { data: "price", width: "15%" },
+            { data: "author", width: "15%" },
+        ]
+    });
+}
