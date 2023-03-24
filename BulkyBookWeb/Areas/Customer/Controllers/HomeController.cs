32efe97870607c0bb24be2f8532abb498ec3ca91
NICOLO' DE PIETRO <NICOLO.DEPIETRO@issgreppi.it>
2023-03-24 11:44:17 +0100

    fino a prima di 1.2.13.8.3

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BulkyBook.Models1;
+﻿using BulkyBook.DataAccess1.Repository.IRepository;
+using BulkyBook.Models1;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,14 +7,24 @@ namespace BulkyBookWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+
     }
+    //public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+    //{
+    //    _logger = logger;
+    //    _unitOfWork = unitOfWork;
+    //}
 
     public IActionResult Index()
     {
+        //IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: 
+        "Category,CoverType");
+        //return View(productList);
         return View();
     }
 
